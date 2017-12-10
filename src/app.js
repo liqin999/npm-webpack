570c11c496d1ea9c43318e44576a9c5527c9f71b
liqin <1483510623@qq.com>
2017-12-10 15:02:59 +0800

    模块化加载一个文件

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import nav from './components/nav.js'// 引入模块
 
 import {firstName,lastName,year} from './components/const.js'
 
-import aa from './components/a.js';
+/*引入一个模块时候，名字要和导出模块名字相同*/
+import { city, Counter } from './components/a.js';
 
 nav();//执行
 
@@ -14,4 +15,5 @@ function setName(){
 }
 setName();
 
-console.log(aa())
+console.log(city);//执行另一个模块里面声明的变量和函数
+console.log(Counter())
--- src/components/a.js
+++ src/components/a.js
@@ -1,3 +1,12 @@
-export default function a() {
-	return "this is a page";
+
+export let city = {
+	"key1":"北京",
+	"key2":"上海",
+	"key3":"南京"
+};
+
+export function Counter() {
+  return "this is a page";
 }
+
+/*使用export 导出一个函数个一个变量的形式*/
